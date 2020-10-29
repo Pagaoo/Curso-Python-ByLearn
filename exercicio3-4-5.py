7100b1e9b2ecc05186e2de17f259195754c86a3d
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-10-29 06:24:45 -0300

    Exercicios de subtração, divisão e multiplicação

---
+++ exercicio3-4-5.py
@@ -0,0 +1,27 @@
+def multiplicacao():
+  print('Multiplicação de dois números')
+  x = float(input('Digite o primeiro número: '))
+  y = float(input('Digite o segundo número: '))
+  mult = x * y
+  print('A multiplicação de',x, 'e',y, 'é de:', mult)
+multiplicacao()
+
+print('------------------------------------')
+
+def divisao():
+  print('Divisão de dois números')
+  a = float(input('Digite o primeiro número: '))
+  b = float(input('Digite o segundo número: '))
+  div = a / b
+  print('A divisão de',a, 'com',b, 'é de: ', div)
+divisao()
+
+print('-----------------------------------------')
+
+def subtracao():
+  print('Subtração de dois números')
+  q = float(input('Digite o primeiro número: '))
+  w = float(input('Digite o segundo número: '))
+  sub = q - w
+  print('A subtração de',q, 'com',w, 'é de', sub)
+subtracao()
