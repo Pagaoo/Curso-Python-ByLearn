c6599897e7e22398f525ae46908de939b34f32d2
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-10-29 06:04:29 -0300

    Update Exercicio2.py

--- Exercicio2.py
+++ Exercicio2.py
@@ -17,5 +17,21 @@ def main():
   b = 87
   soma = a + b
   print('A soma de',a, 'e', b, 'é:', soma)
-
 main()
+
+print('--------------------------------')
+
+def main1():
+  a = float(input('Defina o primeiro numero: '))
+  b = float(input('Defina o segundo numero: '))
+  soma = a + b
+  print('A soma de',a, 'com', b, 'é de: ', soma)
+main1()
+
+print('-------------------------------------')
+
+def main2():
+  nome = input('Digite seu nome: ')
+  sobrenome = input('Digite seu sobrenome: ')
+  print('Seu nome é:', nome, sobrenome)
+main2()
