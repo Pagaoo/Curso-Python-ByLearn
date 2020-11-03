fa03811db6b46415e63e128fca1786cf32dd31d4
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-11-03 18:23:55 -0300

    Update Listas.py

--- Listas.py
+++ Listas.py
@@ -10,3 +10,8 @@ lista = [1,2,3,4,5]
 lista2 = ['a', 'b', 'c', 'd', 'e']
 list3 = ['a', 'b', 2, 3] ##Permite listas mistas
 lista_alunos = [] ##Inicialização de lista vazia
+
+#Há também o método de criação de listas built in
+lista = list() ##Equivale a uma lista vazia, lista = []
+var = 12
+lista_elementos = list(['a', 2, var])
