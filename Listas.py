2a5e4547d9b532d79f9c1f4a196635c32f86c6c4
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-11-03 18:20:25 -0300

    Update Listas.py

--- Listas.py
+++ Listas.py
@@ -5,3 +5,8 @@ def main():
   alunos.append('Roberta')
   print(alunos)
 main()
+
+lista = [1,2,3,4,5]
+lista2 = ['a', 'b', 'c', 'd', 'e']
+list3 = ['a', 'b', 2, 3] ##Permite listas mistas
+lista_alunos = [] ##Inicialização de lista vazia
