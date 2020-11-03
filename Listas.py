f2fed72e387df19ab5d95f3895aa5993bababf3c
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-11-03 18:51:02 -0300

    Update Listas.py

--- Listas.py
+++ Listas.py
@@ -24,3 +24,18 @@ list_b = list_a[:] ##Equanto na linha abaixo eu copio uma lista para a outra, aq
 list_b.append('e')
 print(f'Lista_a: {list_a}')
 print(f'Lista b: {list_b}')
+
+print('---------------------------------------------')
+#Mostrando os elementos da lista, um por um e fatiando listas
+listaA = list([1,2,3,4,5,6,7,8,9,10])
+elem1 = listaA[0]
+elem2 = listaA[1]
+elem3 = listaA[2]
+print(elem1, elem2, elem3)
+print('--------------------------------------------')
+teste1 = listaA[1:4] #Mostra elementos da posição 1 até o teto 4, ou seja, mostra do número 2 até o número 4
+teste2 = listaA[:7] #Mostra elementos de todas as posições até a sétima posição, mesma coisa, a posição 7 não entra pois é o teto final
+teste3 = listaA[4:] #Mostra elementos da posição 4 em diante, até o final da lista
+print(f'teste 1: {teste1}')
+print(f'teste 2: {teste2}')
+print(f'teste 3: {teste3}')
