8d7665ece83a5975f8e51990d61783655ff83980
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-11-03 19:58:05 -0300

    Create Manipulando_strings.py

---
+++ Manipulando_strings.py
@@ -0,0 +1,8 @@
+string = 'Pagão'
+print(string[3])
+##Para mudar um string, podem ser fatiadas, para mudar o valor de algum indice
+nova_string = string[:2] + 'l' + string[3:]
+print(nova_string)
+print('------------------------------------------')
+palavra = 'adcdf'
+print(palavra[-1])##Começa do final e mostra a última letra
