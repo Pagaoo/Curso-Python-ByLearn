e6897c79ef596463fbaf55a44b8e2b1fa9c77459
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-11-03 20:13:14 -0300

    Update Manipulando_strings.py

--- Manipulando_strings.py
+++ Manipulando_strings.py
@@ -6,3 +6,26 @@ print(nova_string)
 print('------------------------------------------')
 palavra = 'adcdf'
 print(palavra[-1])##Começa do final e mostra a última letra
+print(palavra[-2])
+print('--------------------------------------------')
+abc = 'adcbde'
+check = 'b' in abc
+check2 = 'f' not in abc
+print(check)
+print(check2)
+print('------------------------------------------')
+##Concatenar strings
+print('Ga'+'bri'+'el')
+##Alternar upper case and low case
+string = 'Ga'+'bri'.upper()+'el'
+print(string)
+print('--------------------------------------------')
+##tamanho da stirng
+string1 = 'Gabriel'
+print(len(string1))
+print('-----------------------------------------------')
+##Checar se os caracteres são letras
+checando = 'gabriel'.isalpha()
+print(checando)
+checando2 = '1g3e4123gfd'.isalpha()
+print(checando2)
