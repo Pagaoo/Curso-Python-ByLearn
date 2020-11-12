97c7cf125ecea324581e361eb32663dd23826477
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-11-12 20:26:37 -0300

    Create Exercicio7.py

---
+++ Exercicio7.py
@@ -0,0 +1,18 @@
+##Criar uma lista de notas e adicionar 5 á 10 notas a seu critério.
+##Por fim, printar todas as notas dessa lista através de um laço (for)
+
+notas = []
+
+notas.append(1)
+notas.append(2)
+notas.append(3)
+notas.append(4)
+notas.append(5)
+notas.append(6)
+notas.append(7)
+notas.append(8)
+notas.append(9)
+notas.append(10)
+
+for nota in notas:
+    print(nota)
