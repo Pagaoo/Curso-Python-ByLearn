1c551b450743468a183b9c7bbd5c9001fa5a31ad
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-12-10 16:40:45 -0300

    With extra knowledge

--- Blocos_de_Repetição.py
+++ Blocos_de_Repetição.py
@@ -57,3 +57,26 @@ def continue1():
             continue
         print(i)
 continue1()
+
+print('----------------------------------------------')
+
+##EXTRA: Há else em blocos de repetição
+
+def forex():
+    for i in range(5):
+        print(i)
+    else:
+        print('Após laço e iteração')
+forex()
+
+print('----------------------------------------------')
+
+def whileEx():
+    i = 1
+    while i < 5:
+        print(i)
+        i += 1
+    else:
+        print('i não é menor que 5 mais')
+        print(i)
+whileEx()
