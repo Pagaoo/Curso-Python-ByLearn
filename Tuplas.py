e2658c3fa0defeace24bf27f30866718c8009040
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-12-15 16:36:18 -0300

    Update Tuplas.py

--- Tuplas.py
+++ Tuplas.py
@@ -1,5 +1,5 @@
 ##Tuplas são inicializadas com os (), ao contrario das listas que são inicilizadas por []
-
+##Tuplas não tem a opção de adiconar elementos depois de criadas
 def tupla():
     tupla = (1, 2, 3)
     print(tupla)
