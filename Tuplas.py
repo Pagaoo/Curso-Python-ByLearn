298a0022a29e03a4e1d4479746ba42528acfba37
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-12-15 17:54:07 -0300

    Tuples

--- Tuplas.py
+++ Tuplas.py
@@ -23,3 +23,27 @@ lista = [1,2,3]
 tupla = tuple(lista)
 print(type(lista))
 print(type(tupla))
+
+print('----------------------------------------------------------')
+##Para editar tuples, pode transformar a estrutura em uma lista e depois retrasnformar para uma tupla novamente
+##Não é um método recomendado, pois se os dados forem ser mutáveis,, usará lista direto
+tupla1 = (1,2,3,4,5)
+lista1 = list(tupla1)
+lista1.append(8)
+lista1[0] = 'Gabi'
+lista1.remove(3)
+tupla1 = tuple(lista1)
+print(tupla1)
+
+print('----------------------------------------------------------')
+##Entretanto há uma forma de conduzir os dados, como as listas, dicionarios e conjuntos.
+tupla2 = (1,2,3,4,5)
+print('Indice:',tupla2[3])
+print(tupla2[1:3]) ##Exclusivo no final, não mostrará o elemento no indice 3
+print(tupla2[-1])##Mostra os elementos da direita para a esquerda, nesse caso, só mostra o ultimo elemento
+print('O tamanho da tupla é de:',len(tupla2))
+print('O elemento 4, está na tupla?',4 in tupla2)
+print('O elemento 1, não está na tupla?',1 not in tupla2)
+
+for i in tupla2:
+    print('Elemento:',i)
