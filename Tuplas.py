1774ef1cd40fc2cb1ef14550e109fbfacb13ed27
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-12-15 16:30:48 -0300

    Tuples

---
+++ Tuplas.py
@@ -0,0 +1,9 @@
+##Tuplas são inicializadas com os (), ao contrario das listas que são inicilizadas por []
+
+def tupla():
+    tupla = (1, 2, 3)
+    print(tupla)
+    print(tupla[1])
+    print(tupla[0])
+    print(tupla[2])
+tupla()
