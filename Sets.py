5974abe3034ba853c1c09b002b34c34cb5916e19
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-12-16 14:04:01 -0300

    basics os sets

--- Sets.py
+++ Sets.py
@@ -1,4 +1,5 @@
 ##Sets são conhecido por conjuntos e são inicializados com {}, a demais há a execção de criar um conjunto vazio, que seria com os ()
+##Uma particularidade é que os Sets não aceitam duplicatas
 set = {'Conjunto'}
 print(set)
 print(type(set))
