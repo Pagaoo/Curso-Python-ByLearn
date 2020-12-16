e7d42aeb9d53b2ea308a82c23a97a974a4ce50af
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-12-16 14:52:57 -0300

    Sets' basics

--- Sets.py
+++ Sets.py
@@ -14,11 +14,26 @@ print('--------------------------------------------------------')
 ##Usa-se add porque os sets vem das operações matemáticas de conjuntos
 def conjuntos():
     conjunto = {'a','b','c','d'} 
-    conjunto2 = {'j','e','h','x'}
+    conjunto2 = {'a','j','e','h','x','d'}
     print('Conjunto 1:',conjunto)
     print('Conjunto 2:',conjunto2)
     conjunto.add('g')
     conjunto2.add('z')
     print('Conjunto 1 modificado:',conjunto)
     print('Conjunto 2 modificado:',conjunto2)
+    conjunto_total = conjunto.union(conjunto2) ##Une (soma) os conjuntos escolhidos. O union retorna um valor (conjunto)
+    print('Conjunto total:',conjunto_total)
 conjuntos()
+
+print('--------------------------------------------------------')
+
+def frutas():
+    frutas = {'Apple', 'Orange'}
+    frutas.update(['Pineapple', 'Apricot', 'Ackee'])##Adiona uma lista de elementos ao conjunto, também une dois conjuntos, mas adiciona direto ao conjuto. Ex: frutas.uptade(frutas2)
+    frutas.remove('Apple') ##Remove uma chave do conjunto, se o elemento não existir dará erro
+    frutas.discard('Pineapple')##Usando o discard, se o elemento não existir mais, não dará erro
+    frutas.clear()##Limpa o conjunto e retorna um set(), ou seja, conjunto vazio
+    print(frutas)
+frutas()
+
+print('--------------------------------------------------------')
