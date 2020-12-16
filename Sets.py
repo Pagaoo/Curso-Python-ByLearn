3b0eade9a6e321cf7ac150b5d5186133c9ad49e4
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-12-16 14:01:51 -0300

    Learning the basisc os sets

---
+++ Sets.py
@@ -0,0 +1,4 @@
+##Sets são conhecido por conjuntos e são inicializados com {}, a demais há a execção de criar um conjunto vazio, que seria com os ()
+set = {'Conjunto'}
+print(set)
+print(type(set))
