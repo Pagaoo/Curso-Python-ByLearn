6d956db7f7ccba4454f5fb05ec3b640e4608af64
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-12-16 14:33:25 -0300

    Sets' basics

--- Sets.py
+++ Sets.py
@@ -3,3 +3,22 @@
 set = {'Conjunto'}
 print(set)
 print(type(set))
+
+print('--------------------------------------------------------')
+##Prova de que não aceita duplicatas
+conjunto = {1,1,3,4,5,5}
+print(conjunto)
+
+print('--------------------------------------------------------')
+##Não são ordenados
+##Usa-se add porque os sets vem das operações matemáticas de conjuntos
+def conjuntos():
+    conjunto = {'a','b','c','d'} 
+    conjunto2 = {'j','e','h','x'}
+    print('Conjunto 1:',conjunto)
+    print('Conjunto 2:',conjunto2)
+    conjunto.add('g')
+    conjunto2.add('z')
+    print('Conjunto 1 modificado:',conjunto)
+    print('Conjunto 2 modificado:',conjunto2)
+conjuntos()
