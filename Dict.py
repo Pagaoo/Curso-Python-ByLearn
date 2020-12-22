e5e1a293a59c0ac2416872f9523c483762c473e0
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-12-22 16:48:37 -0300

    Dict basics

--- Dict.py
+++ Dict.py
@@ -15,3 +15,13 @@ def dict2():
     print(dict['bird']) ##Não suporta o acesso por índices, mas sim pelas chaves
     print(dict.get('bird')) ##Outra maneira de acessar os valores da dict
 dict2()
+
+print('-----------------------------------------------------------')
+
+def dict3():
+    dict = {'palavra':1, 'palavra2':2.8, 'palavra3':'palara4'} ##Pode usar numeros nas dicts como valores
+    print(dict)
+    dict1 = {1:'um',2:'dois',3:'tres',4:'quatro'}##E também numeros nas chaves
+    print(dict1)
+    print(dict1[1])##Apesar de parecer um índice, está buscando pela CHAVE denominada '1'
+dict3()
