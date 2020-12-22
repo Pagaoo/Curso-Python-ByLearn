4cd4414ce1994e25242a854c4b2c71db039c6da5
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-12-22 16:25:34 -0300

    Dict basics

--- Dict.py
+++ Dict.py
@@ -1,5 +1,6 @@
 ##Os dicionarios são compostos por suas chaves e respectivos valores
 ##Não aceitam duplicatas, e se houver chaves iguais com valores diferentes, o valor apresentado é o ultimo colocado
+##É uma estrutura mutável
 
 def dict1():
     dict = {'dog':'cachorro', 'cat':'gato', 'dog':'cachorro', 'dog':'peixe'}
