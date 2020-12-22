c849ed84061bc6dfb741818f805fbad23e1dd9fc
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-12-22 17:02:23 -0300

    Dict basics

--- Dict.py
+++ Dict.py
@@ -28,3 +28,25 @@ def dict3():
     for i in range(1,5):##Printa os valores com base nas chaves da dict1
         print(dict1[i])
 dict3()
+
+print('-----------------------------------------------------------')
+
+def dict4():
+    dict = {'dog':'cachorro', 'cat':'gato', 'fish':'peixe'}
+
+    for chave in dict:##Printa as chaves da dict
+        print(chave)
+    print('-'*25)
+    for valores in dict:##Printa os valores pertencentes as chaves
+        print(dict[valores])
+    print('-'*25)
+    print('Todas as chaves:',dict.keys())##Método que mostra as chaves presentes na dict
+    print('Todos os valores:',dict.values())##Método que mostra os valores presentes na dict
+    print('-'*25)
+    ##É possivel integrar esses métodos em funções de loop, como o for
+    for chaves in dict.keys():
+        print(chaves)
+    print('-'*25)
+    for valores in dict.values():
+        print(valores)
+dict4()
