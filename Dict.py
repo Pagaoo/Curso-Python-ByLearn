53eafaaee31a0a5f3d1b271c9fb9622e36bf1550
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2020-12-22 16:20:52 -0300

    Dict basics

---
+++ Dict.py
@@ -0,0 +1,7 @@
+##Os dicionarios são compostos por suas chaves e respectivos valores
+
+def dict1():
+    dict = {'dog':'cachorro', 'cat':'gato', 'dog':'cachorro'}
+    print(dict)
+    print(type(dict))
+dict1()
