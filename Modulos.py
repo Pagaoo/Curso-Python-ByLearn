b867d5672f597817d518b773bfa295323586257f
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-01-18 18:23:46 -0300

    Modulos

---
+++ Modulos.py
@@ -0,0 +1,7 @@
+# Importa toda  biblioteca
+import math
+import random
+
+# Importa so a função pow da biblioteca math, no caso, não se usa mais math()
+# só usa-se a funnção importada pow()
+from math import pow
