53e4b53f415121d6da3e59459df6ee28eb17d71c
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-01-19 19:34:08 -0300

    Update Modulos.py

--- Modulos.py
+++ Modulos.py
@@ -5,3 +5,20 @@ import random
 # Importa so a função pow da biblioteca math, no caso, não se usa mais math()
 # só usa-se a funnção importada pow()
 from math import pow
+
+# Modulo math, o principal das bibliotecas de matemática
+# Fatorial de um numero:
+fat = math.factorial(5)
+# Raiz quadrada de um número:
+raiz = math.sqrt(25)
+# Cosseno de um número em radianos:
+cos = math.cos(906)
+# Valor de pi:
+pi = math.pi
+# Valor de Euler:
+e = math.e
+print(f'Fatorial de 5 = {fat}')
+print(f'Raiz de 25 = {raiz}')
+print(f'O cosseno de 90 em radianos é: {cos}')
+print(f'Valor de pi = {pi}')
+print(f'Valor de Euler = {e}')
