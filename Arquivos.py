26ba58321cd978c1d2b2658c236c9bdce5bfeecf
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-02-02 19:08:47 -0300

    Arquivos

--- Arquivos.py
+++ Arquivos.py
@@ -1,1 +1,7 @@
-f = open('arquivo.txt', 'r')  # r -> Read
+# Se o arquivo estiver em outra pasta, dar o caminho dessa pasta
+f = open('Arquivos//arquivo.txt', 'r')  # r -> Read = lê o que está no arquivo
+print(f)
+f = open('Arquivos//arquivo.txt', 'a')  # a -> Append = Lê e adiciona texto
+print(f)
+f = open('Arquivos//arquivo.txt', 'w')  # w -> Write = Escreve no arquivo
+print(f)
