4331b5a9af9525d1d3d263fc20b15a6d3c6453e4
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-02-02 19:11:09 -0300

    Arquivos

--- Arquivos.py
+++ Arquivos.py
@@ -3,5 +3,6 @@ f = open('Arquivos//arquivo.txt', 'r')  # r -> Read = lê o que está no arquivo
 print(f)
 f = open('Arquivos//arquivo.txt', 'a')  # a -> Append = Lê e adiciona texto
 print(f)
-f = open('Arquivos//arquivo.txt', 'w')  # w -> Write = Escreve no arquivo
+# f = open('Arquivos//arquivo.txt', 'w')  # w -> Write = Escreve no arquivo, sobre escreve o arquivo
 print(f)
+f = open('Arquivos//arquivo2.txt', 'w+')  # Cria um arquivo caso não exista
