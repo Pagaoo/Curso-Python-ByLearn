dc38b1eeee8d99c27f1f3bbd6215982ee5a64aa9
Gabriel Paganini <56180950+Pagaoo@users.noreply.github.com>
2021-02-04 18:20:33 -0300

    Lendo arquivos

--- Lendo_Arquivos.py
+++ Lendo_Arquivos.py
@@ -3,7 +3,7 @@ print(f)
 
 print(f.read())  # Vai ler todo o arquivo
 print(type(f.read()))  # f.read() é um str
-# O f.readline() lê uma linha por execução. Existe também o redlines() que transformas os items do arquivo em uma lista
+# O f.readline() lê uma linha por execução. Existe também o readlines() que transforma os items do arquivo em uma lista
 f = open('Arquivos//arquivo2.txt')
-linhas = f.readlines()
+linhas = f.readlines()  # a função retorna as linhas restantes do arquivo, caso já tenham sido lidas todas, retorna uma lista vazia
 print(linhas)
